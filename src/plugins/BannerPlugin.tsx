e8506415a8fe1df27f6675569a9a76322a9db4cc
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-10-30 13:26:49 +0100

    import + exportJSON

--- src/plugins/BannerPlugin.tsx
+++ src/plugins/BannerPlugin.tsx
@@ -1,5 +1,6 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import {
+  SerializedLexicalNode,
   LexicalEditor,
   NodeKey,
   COMMAND_PRIORITY_LOW,
@@ -11,11 +12,14 @@ import {
   ElementNode,
   $createParagraphNode,
   RangeSelection,
+  SerializedElementNode,
 } from 'lexical';
 
 import { $setBlocksType } from '@lexical/selection';
 import { deckContext } from '../components/EditorPage/EditorPage';
 import { useContext } from 'react';
+
+type SerializedBannerNode = { cardId: number } & SerializedElementNode;
 
 //I suspect this is pretty bad practice but it works for now. I should circle back to this when I understand Lexical better. //Dan 30-10-2023
 let deck: undefined | any[];
@@ -27,13 +31,14 @@ export class BannerNode extends ElementNode {
     return 'banner';
   }
 
-  constructor(key?: NodeKey) {
+  constructor(cardId = -1, key?: NodeKey) {
     super(key);
-    this.__cardId = 1000;
+    this.__cardId = cardId;
   }
 
   //Lots of testing functionality for now. Should just set __cardId //Dan 30-10-2023
   setCardId(cardId: number | false) {
+    // editor.registerMutationListener(BannerNode, () => {});
     const self = this.getWritable();
     self.__cardId = this.__cardId + 1;
     console.log(deck);
@@ -48,15 +53,35 @@ export class BannerNode extends ElementNode {
     }
   }
 
+  getCardId(): number {
+    return this.__cardId;
+  }
+
   static clone(node: BannerNode): BannerNode {
-    return new BannerNode(node.__key);
+    return new BannerNode(undefined, node.__key);
   }
 
   //Working on this now //Dan 30-10-2023
-  // static importJSON() {}
-  // exportJSON(): SerializedLexicalNode {
-  //   return this.exportJSON();
-  // }
+  exportJSON(): SerializedBannerNode {
+    console.log(self);
+    console.log(this); //For some reason the __cardId field has its value from the constructor inside of this function but not in the onClick... I think I may be updating it incorrectly.
+    console.log(this.getCardId());
+    // console.log('BannerNode exported. cardId = ' + this.getCardId());
+    return {
+      ...super.exportJSON(),
+      type: 'banner',
+      cardId: this.__cardId,
+    };
+  }
+  static importJSON(serializedNode: SerializedBannerNode): LexicalNode {
+    console.log('Bannernode imported');
+    const node = $createBannerNode(serializedNode.cardId);
+
+    node.setFormat(serializedNode.format);
+    node.setIndent(serializedNode.indent);
+    node.setDirection(serializedNode.direction);
+    return node;
+  }
 
   //This is where you create the DOM element for the node, here we should
   createDOM(config: EditorConfig): HTMLElement {
@@ -71,6 +96,7 @@ export class BannerNode extends ElementNode {
       //This is very WIP and testing functionality atm. Will be looked over and fixed. //Dan 30-10-2023
       editor.update(() => this.setCardId(0));
       console.log(this.__cardId);
+      console.log(this);
 
       // This would be how we could make things happen depending on written text etc. Potenially relevant. //Dan 30-10-2023
       // editorTest[0].registerNodeTransform(BannerNode, bannerNode =>
@@ -107,8 +133,11 @@ export class BannerNode extends ElementNode {
   }
 }
 
-export function $createBannerNode(): BannerNode {
-  return new BannerNode(undefined);
+export function $createBannerNode(
+  cardId: number | undefined,
+  key?: string,
+): BannerNode {
+  return new BannerNode(cardId, key);
 }
 
 // If you need this functionality you can uncomment it
@@ -132,7 +161,7 @@ export function BannerPlugin(): null {
     () => {
       const selection = $getSelection();
       if ($isRangeSelection(selection)) {
-        $setBlocksType(selection, () => $createBannerNode());
+        $setBlocksType(selection, () => $createBannerNode(undefined));
       }
       return true;
     },
