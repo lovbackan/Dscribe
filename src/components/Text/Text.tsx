515b7ed70ff554f61d6698c0a28126bfd35fa86d
lovbackan <simon@lovbacka.se>
2023-11-01 11:13:42 +0100

    removed if else solution of textcomponent

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -19,40 +19,41 @@ interface TextProps {
 
 //FC is a type that ships with React's TypeScript types. It represents the type of a functional component, which is the building block of most modern React apps. tsx. // Component without props. const Component : React .
 export const Text: React.FC<TextProps> = ({ content, variant, textColor }) => {
-  const textConverter = (variant: TextType, textColor: TextColor) => {
-    let tailwindColor = '';
-    if (textColor === 'black') {
-      tailwindColor = 'text-black';
-    } else if (textColor === 'white') {
-      tailwindColor = 'text-white';
-    } else if (textColor === 'green') {
-      tailwindColor = 'text-green-700';
-    } else {
-      tailwindColor = 'text-pink';
-    }
-
-    if (variant === 'heading1') {
-      return <h1 className={`text-3xl ${tailwindColor}`}>{content}</h1>;
-    } else if (variant === 'heading1Bold') {
-      return (
-        <h1 className={`text-3xl ${tailwindColor} font-bold`}>{content}</h1>
-      );
-    } else if (variant === 'heading2') {
-      return <h2 className={`text-2xl ${tailwindColor}`}>{content}</h2>;
-    } else if (variant === 'heading3') {
-      return <h3 className={`text-3xl ${tailwindColor}`}>{content}</h3>;
-    } else if (variant === 'p-primary') {
-      return <p className={tailwindColor}>{content}</p>;
-    } else if (variant === 'p-secondary') {
-      return <p className={tailwindColor}>{content}</p>;
-    } else if (variant === 'cardTitle') {
-      return (
-        <h4 className={`text-xl ${tailwindColor} break-words`}>{content}</h4>
-      );
-    } else {
-      return <p className={` text-sm ${tailwindColor}`}>{content}</p>;
-    }
+  const textColorClasses = {
+    black: 'text-black',
+    white: 'text-white',
+    green: 'text-green-700',
+    pink: 'text-pink',
+    default: 'text-pink',
   };
 
-  return textConverter(variant, textColor);
+  const tailwindColorClass =
+    textColorClasses[textColor] || textColorClasses.default;
+
+  const tailwindClasses = {
+    heading1: `text-3xl ${tailwindColorClass}`,
+    heading1Bold: `text-3xl ${tailwindColorClass} font-bold`,
+    heading2: `text-2xl ${tailwindColorClass}`,
+    heading3: `text-3xl ${tailwindColorClass}`,
+    'p-primary': tailwindColorClass,
+    'p-secondary': tailwindColorClass,
+    cardTitle: `text-xl ${tailwindColorClass} break-words`,
+    default: `text-sm ${tailwindColorClass}`,
+  };
+
+  const className = tailwindClasses[variant] || tailwindClasses.default;
+
+  const elementType =
+    {
+      heading1: 'h1',
+      heading1Bold: 'h1',
+      heading2: 'h2',
+      heading3: 'h3',
+      'p-primary': 'p',
+      'p-secondary': 'p',
+      cardTitle: 'h4',
+      default: 'p',
+    }[variant] || 'p';
+
+  return React.createElement(elementType, { className }, content);
 };
