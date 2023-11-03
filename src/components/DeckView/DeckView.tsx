338fb92ad2f9ba8b2cf3e7251a5e3d35e7e3f2d9
lovbackan <simon@lovbacka.se>
2023-11-03 17:45:02 +0100

    close button css deckview

--- src/components/DeckView/DeckView.tsx
+++ src/components/DeckView/DeckView.tsx
@@ -24,74 +24,78 @@ export const DeckView: React.FC<DeckViewProps> = (props: DeckViewProps) => {
   return (
     <div
       id="deckView"
-      className="  bg-slate-500 overflow-y-auto rounded-3xl px-20 w-[70%] h-[85vh]"
+      className="  bg-slate-500 overflow-y-auto rounded-3xl  w-[70%] h-[85vh]"
     >
-      <div className=" flex flex-row justify-end ">
-        <CTAButton
-          title="X"
-          variant="primary"
-          onClick={() => {
-            console.log('close deck view');
-            props.toggleDeckView();
-          }}
-        />
+      <div className="flex flex-row justify-end ">
+        <div className="fixed pr-6 pt-3">
+          <CTAButton
+            title="X"
+            variant="minimize/close"
+            onClick={() => {
+              console.log('close deck view');
+              props.toggleDeckView();
+            }}
+          />
+        </div>
       </div>
-      <div id="title" className="mt-10 ">
-        <h1>DeckView</h1>
-      </div>
-      {/* map over deck and display cards */}
+      <div className="px-20">
+        <div id="title" className="mt-10 ">
+          <h1>DeckView</h1>
+        </div>
+        {/* map over deck and display cards */}
 
-      <div className="flex flex-row">
-        <Text variant="heading2" content="Filter" textColor="black" />
-        <Input
-          variant="primary"
-          placeholder="Search"
-          onChange={() => {
-            console.log('u are writing here');
-          }}
-        />
-      </div>
+        <div className="flex flex-row">
+          <Text variant="heading2" content="Filter" textColor="black" />
+          <Input
+            variant="primary"
+            placeholder="Search"
+            onChange={() => {
+              console.log('u are writing here');
+            }}
+          />
+        </div>
 
-      <div className="flex flex-row w-[100%]">
-        {props.deck.map(card => {
-          return (
-            <CTAButton
-              title={card.category_id}
-              variant="deckViewCategory"
-              onClick={() => {
-                console.log(card.category_id);
-              }}
-            />
-          );
-        })}
-      </div>
+        <div className="flex flex-row w-[100%]">
+          {props.deck.map(card => {
+            return (
+              <CTAButton
+                title={card.category_id}
+                variant="deckViewCategory"
+                onClick={() => {
+                  console.log(card.category_id);
+                }}
+              />
+            );
+          })}
+        </div>
 
-      <div className="flex flex-row pb-10">
-        <Text textColor="black" variant="heading2" content="Chapters" />
+        <div className="flex flex-row pb-10">
+          <Text textColor="black" variant="heading2" content="Chapters" />
 
-        <div className="w-full h-0.5 bg-black self-end"></div>
-      </div>
-      <div className="flex flex-row pb-10">
-        <Text textColor="black" variant="heading2" content="Cards" />
+          <div className="w-full h-0.5 bg-black self-end"></div>
+        </div>
+        <div className="flex flex-row pb-10">
+          <Text textColor="black" variant="heading2" content="Cards" />
 
-        <div className="w-full h-0.5 bg-black self-end"></div>
-      </div>
-      <div className="flex flex-wrap gap-10 my-2 justify-center items-center">
-        {props.deck.map(card => {
-          return (
-            <Card
-              key={card.id}
-              supabase={props.supabase}
-              card={card}
-              setSelectedCard={props.setSelectedCard}
-              deck={props.deck}
-              setDeck={props.setDeck}
-              hand={props.hand}
-              setHand={props.setHand}
-              variant="deckCard"
-            />
-          );
-        })}
+          <div className="w-full h-0.5 bg-black self-end"></div>
+        </div>
+        <div className="flex flex-wrap gap-10 my-2 justify-center items-center">
+          {props.deck.map(card => {
+            return (
+              <Card
+                key={card.id}
+                supabase={props.supabase}
+                card={card}
+                setSelectedCard={props.setSelectedCard}
+                deck={props.deck}
+                setDeck={props.setDeck}
+                hand={props.hand}
+                setHand={props.setHand}
+                variant="deckCard"
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
