2c3477ebca3ac8465868763bdfdb8f34357e5688
lovbackan <simon@lovbacka.se>
2023-11-06 14:32:48 +0100

    DeckView has vw now

--- src/components/Forms/FormCard.tsx
+++ src/components/Forms/FormCard.tsx
@@ -1,3 +1,4 @@
+import { Text } from '../Text/Text';
 interface FormCardProps {
   formComponent: React.ReactNode;
 }
@@ -7,17 +8,17 @@ const FormCard: React.FC<FormCardProps> = props => {
   return (
     <div
       id="outerContainer"
-      className="w-[400px] h-[300px] rounded-[20px] flex-row bg-cyan-200 flex"
+      className="w-[690px] h-[300px] rounded-[20px] flex-row bg-cyan-200 flex"
     >
       <div
         id="logoContainer"
-        className="w-[200px] h-full rounded-l-[20px] bg-slate-500 flex justify-center items-center"
+        className="w-[200px] h-full rounded-[20px] bg-slate-500 flex justify-center items-center"
       >
-        <h1 className="text-xl">Dscribe</h1>
+        <Text variant="heading2" textColor="black" content="Codeck" />
       </div>
       <div
         id="infoContainer"
-        className="w-[200px] h-[300px] rounded-r-[20px] bg-slate-400 flex justify-center items-center flex-col"
+        className="w-[490px] h-full rounded-r-[20px] bg-slate-400 flex justify-center items-center flex-col"
       >
         {formComponent}
       </div>
