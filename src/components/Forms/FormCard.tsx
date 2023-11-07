4a98969839eb0225eae3c4691ea4a203e8fe0082
lovbackan <simon@lovbacka.se>
2023-11-07 12:34:55 +0100

    Login logo variant added

--- src/components/Forms/FormCard.tsx
+++ src/components/Forms/FormCard.tsx
@@ -11,12 +11,8 @@ const FormCard: React.FC<FormCardProps> = props => {
       id="outerContainer"
       className="w-[690px] h-[300px] rounded-[20px] flex-row bg-inherit flex border-2 border-white"
     >
-      <div
-        id="logoContainer"
-        className="w-[200px] h-full rounded-[20px] bg-inherit flex justify-center items-center border-white border-r-2"
-      >
-        <Text variant="logoBig" textColor="white" content="Codeck" />
-      </div>
+      <Logo variant="login" />
+
       <div
         id="infoContainer"
         className="w-[490px] h-full rounded-r-[20px] bg-inherit flex justify-center items-center flex-col"
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,6 @@
 import { Text } from '../Text/Text';
 
-type LogoType = 'big' | 'medium' | 'small';
+type LogoType = 'big' | 'medium' | 'small' | 'login';
 
 interface LogoProps {
   variant: LogoType;
@@ -9,6 +9,8 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ variant }) => {
   const logoClasses = {
     big: 'h-[300px] w-[200px] rounded-3xl border-2 border-white flex flex-col justify-center items-center ',
+    login:
+      'w-[200px] w-[200px] rounded-[20px] bg-inherit flex justify-center items-center border-white border-r-2 ',
     medium:
       'h-[200px] w-[133px] rounded-3xl border-2 border-white flex flex-col justify-center items-center',
     small:
@@ -18,6 +20,12 @@ const Logo: React.FC<LogoProps> = ({ variant }) => {
   const className = logoClasses[variant] || logoClasses.big;
 
   if (variant === 'big') {
+    return (
+      <div id="logo" className={`${className}`}>
+        <Text variant="logoBig" textColor="white" content="Codeck" />
+      </div>
+    );
+  } else if (variant === 'login') {
     return (
       <div id="logo" className={`${className}`}>
         <Text variant="logoBig" textColor="white" content="Codeck" />
